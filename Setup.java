9bf421b3d17ec03a14d37c5bb78bf45d479cc961
Xander <contact@xander479.co.uk>
2020-03-25 01:13:43 +0000

    Create Setup.java
    This will eventually allow you to enter your own keys/tokens to use it yourself

---
+++ Setup.java
@@ -0,0 +1,8 @@
+package xander479;
+
+class Setup {
+	final static String CONSUMER_KEY = "XXXXXXXXXXXXXXX";
+	final static String CONSUMER_KEY_SECRET = "XXXXXXXXXXXXXXX";
+	final static String ACCESS_TOKEN = "XXXXXXXXXXXXXXX";
+	final static String ACCESS_TOKEN_SECRET = "XXXXXXXXXXXXXXX";
+}
