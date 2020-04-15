d0bf9ff04572de373b8c134df589a070c369b934
Xander Clinton <contact@xander479.co.uk>
2020-04-16 00:11:25 +0100

    Now using xml files to store configuration stuff

--- Setup.java
+++
@@ -1,8 +0,0 @@
-package xander479;
-
-class Setup {
-	final static String CONSUMER_KEY = "XXXXXXXXXXXXXXX";
-	final static String CONSUMER_KEY_SECRET = "XXXXXXXXXXXXXXX";
-	final static String ACCESS_TOKEN = "XXXXXXXXXXXXXXX";
-	final static String ACCESS_TOKEN_SECRET = "XXXXXXXXXXXXXXX";
-}
